d3713bf66f21e37e7d01767ca2aadb9a639402f4
Christian Klein <cklein70@gmail.com>
2022-05-23 22:58:23 -0700

    Cleaned up Styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import { Grid } from "@mui/material";
 import BgPanel from "./components/BgPanel";
-import myGames from './data.json'
+import myGames from "./data.json";
+import Box from "@mui/material/Box";
 
 function App() {
-  
   return (
-    <div>
-      <h1>My Games</h1>
-
-      {myGames && 
-        myGames.map(({id,name,url, image}) => (
-        <div key={id}>
-          <BgPanel title={name} rulesUrl={'/rules/'+url} image={'/img/'+image} />
-        </div>
-      ))}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        {myGames &&
+          myGames.map(({ id, name, url, image, minPlayers, maxPlayers }) => (
+           
+              <Grid item xs={12} md={4} key={id}>
+                <BgPanel
+                  title={name}
+                  rulesUrl={"/rules/" + url}
+                  image={"/img/" + image}
+                  minPlayers={minPlayers}
+                  maxPlayers={maxPlayers}
+                />
+              </Grid>
+           
+          ))}
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/BgPanel.js
+++ src/components/BgPanel.js
@@ -1,38 +1,32 @@
-import { useState } from 'react';
 import QRCode from "react-qr-code";
-import Backdrop from './Backdrop';
-import Modal from './Modal';
+import { Card, CardHeader } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-function BgPanel(props) {
-    var pathToRules = window.location.origin + props.rulesUrl;
+const BgPanel = (props) => {
+  var pathToRules = window.location.origin + props.rulesUrl;
 
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    function deleteHandler() {
-        setModalIsOpen(true);
-    }
-
-    function closeHandler() {
-        setModalIsOpen(false);
-    }
-
-    return (
-        <div className="card">
-            <h2>{props.title}</h2>
-            <a href={pathToRules}>
-                <img src={props.image} alt={props.title} className="imgCard" />
-                <QRCode value={pathToRules} />
-            </a>
-            <div className="actions">
-                <button className="btn" onClick={deleteHandler}>Delete</button>
-            </div>
-
-
-            {modalIsOpen && <Modal onCancel={closeHandler} onConfirm={closeHandler} />}
-            {modalIsOpen && <Backdrop onClick={closeHandler} />}
-
-        </div>
-    );
-}
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardHeader title={props.title} />
+      <CardContent>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.minPlayers}-{props.maxPlayers} Players
+        </Typography>
+        <Typography variant="body2">
+          <a href={pathToRules}>
+            <img src={props.image} alt={props.title} className="imgCard" />
+            <QRCode value={pathToRules} />
+          </a>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">BGG</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default BgPanel;
